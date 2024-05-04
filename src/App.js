92788e590de2f58e19a89ab58be90189c411258b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-05-04 07:08:24 -0700

    Put your messsage here

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import MemeGen from "./components/MemeGen";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <MemeGen />
+    </>
+  );
+}
